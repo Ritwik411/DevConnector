d04fe1b90c3188902bc7cd7e8df58cdd87d6a38d
Ritwik Gupta <ritwik41199@gmail.com>
2022-01-04 12:43:07 +0530

    fixed issues

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,7 +17,7 @@ const Register = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert("Passwords do not match", "danger");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
     }
