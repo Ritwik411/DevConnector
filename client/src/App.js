5758d15c897981bc5d4f1b723d24b60fd6e512f5
Ritwik Gupta <ritwik41199@gmail.com>
2022-01-13 13:13:16 +0530

    Completed Registration redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,23 +9,33 @@ import Alert from "./components/layout/Alert";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
-
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      {/* <Fragment> */}
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Alert />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-      {/* </Fragment> */}
-    </Router>
-  </Provider>
-);
+import setAuthToken from "./utils/setAuthToken";
+import { useEffect } from "react";
+import { loadUser } from "./actions/auth";
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        {/* <Fragment> */}
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+        {/* </Fragment> */}
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
